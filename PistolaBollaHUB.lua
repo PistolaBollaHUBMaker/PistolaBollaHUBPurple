76fb4e371ab55f0caddadefb86d06bd1e873104e
PistolaBollaHUBMaker <111288040+PistolaBollaHUBMaker@users.noreply.github.com>
2024-06-10 16:46:11 +0200

    Update PistolaBollaHUB.lua

--- PistolaBollaHUB.lua
+++ PistolaBollaHUB.lua
@@ -207,7 +207,7 @@ end
 
 function PizdaMata()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/PistolaBollaHUBMaker/PistolaBollaHUBPurple/main/EditedOrion.lua?')))()
-local Window = OrionLib:MakeWindow({HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroIcon = "rbxassetid://17806684890", IntroText = "PistolaBollaHUB", Icon = "rbxassetid://17806684890"})
+local Window = OrionLib:MakeWindow({HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroIcon = "rbxassetid://17806684890", IntroText = "PistolaBollaHUB"})
 local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
 
 
